269926d545751604a957370669c821c55e78006a
denzorn <maxsed0@gmail.com>
2020-05-29 11:26:32 +0300

    Add exceptions in server.py and client.py

--- client.py
+++ client.py
@@ -2,23 +2,37 @@ import pika
 import os
 import sys
 
-host = sys.argv[1]
-credentials = pika.PlainCredentials('guest', 'guest')
-connection = pika.BlockingConnection(pika.ConnectionParameters(host, 5672, '/', credentials))
 
-channel = connection.channel()
-channel.queue_declare(queue='dir_files')
+def client():
+    try:
+        host = sys.argv[1]
+    except IndexError:
+        print('You did not pass arguments. Example: python client.py [server ip address]')
+        return
+    credentials = pika.PlainCredentials('guest', 'guest')
+    try:
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host, 5672, '/', credentials))
+    except:
+        print('Your RabbitMQ not run')
+        return
+    channel = connection.channel()
+    channel.queue_declare(queue='dir_files')
 
-def callback(ch, method, properties, body):
-    os.system('clear')
-    for root, dirs, files in eval(body):
-        level = root.count('/')
-        indent = ' ' * 4 * level
-        print('{}{}/'.format(indent, os.path.basename(root)))
-        subindent = ' ' *4 * (level + 1)
-        for file in files:
-            print('{}{}'.format(subindent, file))
+    def callback(ch, method, properties, body):
+        os.system('clear')
+        for root, dirs, files in eval(body):
+            level = root.count('/')
+            indent = ' ' * 4 * level
+            print('{}{}/'.format(indent, os.path.basename(root)))
+            subindent = ' ' *4 * (level + 1)
+            for file in files:
+                print('{}{}'.format(subindent, file))
 
-channel.basic_consume(queue='dir_files', on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue='dir_files', on_message_callback=callback, auto_ack=True)
 
-channel.start_consuming()
+    channel.start_consuming()
+
+
+if __name__ == '__main__':
+    client()
+
--- server.py
+++ server.py
@@ -3,17 +3,32 @@ import os
 import time
 import sys
 
-directory = sys.argv[1]
-credentials = pika.PlainCredentials('guest', 'guest')
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', 5672, '/', credentials))
-channel = connection.channel()
-channel.queue_declare(queue='dir_files')
 
-list_directory = ''
+def server():
+    try:
+        directory = sys.argv[1]
+    except IndexError:
+        print('You did not pass arguments. Example: python server.py [path you directory]')
+        return
+    credentials = pika.PlainCredentials('guest', 'guest')
+    try:
+        connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', 5672, '/', credentials))
+    except:
+        print('You RabbitMQ not run')
+        return
+    channel = connection.channel()
+    channel.queue_declare(queue='dir_files')
 
-while True:
-    if list_directory != list(os.walk(directory)):
-        list_directory = list(os.walk(directory))
-        channel.basic_publish(exchange='', routing_key='dir_files', body=str(list(list_directory)))
-    time.sleep(5)
+    list_directory = ''
+
+    while True:
+        if list_directory != list(os.walk(directory)):
+            list_directory = list(os.walk(directory))
+            channel.basic_publish(exchange='', routing_key='dir_files', body=str(list(list_directory)))
+        time.sleep(5)
+    return
+
+
+if __name__ == '__main__':
+    server()
  
